b1f042fc5a980b4f792d914eeb39a40aee65dc1d
tomimarkus991 <tomimarkusalber@gmail.com>
2022-01-20 11:24:56 +0200

    WIP roles

--- src/redux/features/role/role.slice.ts
+++ src/redux/features/role/role.slice.ts
@@ -1,5 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+import { CareRole } from "types";
+
+import { StateType } from "redux/features/common";
+
+const initialState: StateType<CareRole> = {
+  entities: [],
+  entity: null,
+  isLoading: false,
+  isSuccess: false,
+  error: {},
+};
+
 export const roleSlice = createSlice({
   name: "role",
   initialState: {},
