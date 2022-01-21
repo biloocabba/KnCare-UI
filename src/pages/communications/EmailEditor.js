252a71996bd29fb57689210c6c18f7c7726c5ab1
tomimarkus991 <tomimarkusalber@gmail.com>
2022-01-21 14:04:24 +0200

    fix lint errors

--- src/pages/communications/EmailEditor.js
+++ src/pages/communications/EmailEditor.js
@@ -43,7 +43,7 @@ import { BoxHeader } from "components/headers";
 
 //import emailService from "services/EmailService";
 
-import "react-quill/dist/quill.snow.css";
+// import "react-quill/dist/quill.snow.css";
 
 var EmailEditor = props => {
   let history = useHistory();
