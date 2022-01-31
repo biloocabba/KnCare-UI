868656a01278216f932a9f3762334b68233d3784
tomimarkus991 <tomimarkusalber@gmail.com>
2022-01-31 15:01:18 +0200

    convert selectfield to TS

---
+++ src/components/widgets/select-field/SelectField.tsx
@@ -0,0 +1,30 @@
+import { FormGroup } from "reactstrap";
+
+import Select from "react-select";
+import makeAnimated from "react-select/animated";
+
+// import StateManagedSelect from "react-select/dist/declarations/src/stateManager";
+
+// type Props = StateManagedSelect &
+//   SingleValueProps &
+//   InputProps & {
+//     id: string;
+//     label: string;
+//   };
+
+// find types
+type Props = any & {
+  id: string;
+  label: string;
+};
+
+export const SelectField = ({ id, label, ...props }: Props) => {
+  return (
+    <FormGroup>
+      <label className="form-control-label" htmlFor={id}>
+        {label}
+      </label>
+      <Select {...props} components={makeAnimated()} />
+    </FormGroup>
+  );
+};
