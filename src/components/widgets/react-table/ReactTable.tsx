eda78d69527c509159cbc027ee0f688d1900adba
tomimarkus991 <tomimarkusalber@gmail.com>
2022-02-21 14:58:13 +0200

    [KNC-145]: Implement Firealert in ReactTable on delete item click

--- src/components/widgets/react-table/ReactTable.tsx
+++ src/components/widgets/react-table/ReactTable.tsx
@@ -5,6 +5,8 @@ import { Button } from "reactstrap";
 import BootstrapTable from "react-bootstrap-table-next";
 import ToolkitProvider, { Search } from "react-bootstrap-table2-toolkit";
 
+import { useFireAlert } from "hooks";
+
 import { pagination, selectRow } from ".";
 
 const { SearchBar } = Search;
@@ -28,7 +30,6 @@ export const ReactTable = <T extends { id: number }>({
   keyField,
   data,
   onViewDetailsClick,
-  onDeleteItemClick,
   selectedRows,
   setSelectedRows,
   searchBarPlaceholder,
@@ -36,6 +37,8 @@ export const ReactTable = <T extends { id: number }>({
   tableRef,
   formatterFn,
 }: Props<T>) => {
+  const { alert, fireAlert } = useFireAlert();
+
   const formatActionButtonCell = (_: any, row: T): React.ReactNode => {
     const rowId = row.id.toString();
     return (
@@ -51,12 +54,13 @@ export const ReactTable = <T extends { id: number }>({
             <i id={rowId} className="ni ni-badge" />
           </span>
         </Button>
+
         <Button
           id={rowId}
           className="btn-icon btn-2"
           color="danger"
           type="button"
-          onClick={onDeleteItemClick}
+          onClick={() => fireAlert()}
         >
           <span id={rowId} className="btn-inner--icon">
             <i id={rowId} className="ni ni-fat-remove" />
@@ -74,41 +78,44 @@ export const ReactTable = <T extends { id: number }>({
   // }
 
   return (
-    <ToolkitProvider data={data} keyField={keyField} columns={columns} bootstrap4 search>
-      {props => (
-        <div className="py-4 table-responsive">
-          <div
-            id="datatable-basic_filter"
-            className="dataTables_filter px-4 pb-1"
-            style={{ display: "flex" }}
-          >
-            <label>
-              {searchBarPlaceholder}
-              <SearchBar className="form-control-sm mr-3" placeholder="" {...props.searchProps} />
-            </label>
+    <>
+      {alert}
+      <ToolkitProvider data={data} keyField={keyField} columns={columns} bootstrap4 search>
+        {props => (
+          <div className="py-4 table-responsive">
+            <div
+              id="datatable-basic_filter"
+              className="dataTables_filter px-4 pb-1"
+              style={{ display: "flex" }}
+            >
+              <label>
+                {searchBarPlaceholder}
+                <SearchBar className="form-control-sm mr-3" placeholder="" {...props.searchProps} />
+              </label>
 
-            {selectButtonText && (
-              <>
-                <Button
-                  className="btn btn-success"
-                  onClick={() => console.log("selectedRows", selectedRows)}
-                >
-                  {selectButtonText}
-                </Button>
-              </>
-            )}
+              {selectButtonText && (
+                <>
+                  <Button
+                    className="btn btn-success"
+                    onClick={() => console.log("selectedRows", selectedRows)}
+                  >
+                    {selectButtonText}
+                  </Button>
+                </>
+              )}
+            </div>
+            <BootstrapTable
+              {...props.baseProps}
+              ref={tableRef}
+              bootstrap4
+              pagination={pagination}
+              bordered={false}
+              selectRow={selectRow(setSelectedRows)}
+            />
           </div>
-          <BootstrapTable
-            {...props.baseProps}
-            ref={tableRef}
-            bootstrap4
-            pagination={pagination}
-            bordered={false}
-            selectRow={selectRow(setSelectedRows)}
-          />
-        </div>
-      )}
-    </ToolkitProvider>
+        )}
+      </ToolkitProvider>
+    </>
   );
 };
 
