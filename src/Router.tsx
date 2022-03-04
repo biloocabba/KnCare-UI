d1fae3dd801fec361f41c971cbcacf28ffe79cdf
tomimarkus991 <tomimarkusalber@gmail.com>
2022-03-04 16:44:20 +0200

    [KNC-183-improvements]: Use Role.Anonymous for better readability

--- src/Router.tsx
+++ src/Router.tsx
@@ -9,22 +9,23 @@ import { LOGIN } from "pages/auth";
 import { HOME } from "pages/home";
 
 import { getRoutes, AdminLayout, AuthLayout } from "layouts";
+import { Role } from "./types";
 
 export const Router = () => {
   const userRole = useAppSelector(selectLoggedUserRole);
 
   return (
     <Routes>
-      {userRole !== 1 && (
+      {userRole !== Role.Anonymous && (
         <Route element={<AdminLayout />}>{getRoutes(routes, "/admin", userRole)}</Route>
       )}
-      {userRole === 1 && (
+      {userRole === Role.Anonymous && (
         <Route element={<AuthLayout />}>{getRoutes(routes, "/auth", userRole)}</Route>
       )}
 
       <Route
         path="*"
-        element={<Navigate to={userRole !== 1 ? `admin${HOME}` : `auth${LOGIN}`} replace />}
+        element={<Navigate to={userRole !== Role.Anonymous ? `admin${HOME}` : `auth${LOGIN}`} replace />}
       />
     </Routes>
   );
